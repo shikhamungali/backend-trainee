f77d22c2979ad0c5d235acdf053e7e3854d60251
shikha <shikha1081998@gmail.com>
2022-08-10 12:45:39 +0530

    post-api problem 1

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://Shikhamungali:4t2GRX51VoOU37yQ@cluster0.8hmxve1.mongodb.net/Shikha-Mungali?retryWrites=true&w=majority" ,{
+    useNewUrlParser: true
+})
+.then(() => console.log("MongoDB is connected"))
+.catch( err => console.log(err))
+
 app.use('/', route);
+
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,29 +1,49 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
 const router = express.Router();
 
-router.get("/solutionOne", function (req, res) {
-    let arr = [1, 2, 3, 4, 5, 7, 8, 9]
-    let sumOfArr = 1;
-    for (let n = 2; n <= 9; n++) {
-        sumOfArr = sumOfArr + n
-        sumOfArr = sumOfArr - arr[n - 2]
-    }
-    let missingNo = sumOfArr
-    res.send(missingNo + " " + "is the missing no.")
-})
+let players =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ]
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ]
+        },
+    ]
 
-router.get('/solutionTwo', function (req, res) {
-    let arr = [34, 35, 36, 38, 39]
-    let sumOfArr = 34;
-    for (let n = 35; n <= 39; n++) {
-        sumOfArr = sumOfArr + n
-        sumOfArr = sumOfArr - arr[n - 35]
+
+
+router.post('/players',function(req,res){
+    for(i=0;i<players.length;i++){
+     if (req.body.name == players[i].name){
+           return res.send({data:"ERROR!!!!!!(name should be unique)"})
+        }
     }
-    let missingNo = sumOfArr
-    res.send(missingNo + " " + " is the missing no.")
+
+    let c = req.body
+    players.push(c)
+   return res.send({data:players,status:true})
 })
 
 module.exports = router;
