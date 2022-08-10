d3f6df9122c8624dc1e301ae8344b4c9114bb27d
shikha <shikha1081998@gmail.com>
2022-08-10 12:59:17 +0530

    post-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,50 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
-let players =
-    [
-        {
-            "name": "manish",
-            "dob": "1/1/1995",
-            "gender": "male",
-            "city": "jalandhar",
-            "sports": [
-                "swimming"
-            ]
-        },
-        {
-            "name": "gopal",
-            "dob": "1/09/1995",
-            "gender": "male",
-            "city": "delhi",
-            "sports": [
-                "soccer"
-            ]
-        },
-        {
-            "name": "lokesh",
-            "dob": "1/1/1990",
-            "gender": "male",
-            "city": "mumbai",
-            "sports": [
-                "soccer"
-            ]
-        },
-    ]
+router.get('/students/:name', function (req, res) {
+    let studentName = req.params.name
+    console.log(studentName)
+    res.send(studentName)
+})
+
+router.get("/random", function (req, res) {
+    res.send("hi there")
+})
+
+
+router.get("/test-api", function (req, res) {
+    res.send("hi FunctionUp")
+})
+
+
+router.get("/test-api-2", function (req, res) {
+    res.send("hi FunctionUp. This is another cool API")
+})
+
+
+router.get("/test-api-3", function (req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+})
+
+
+router.get("/test-api-4", function (req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
 
 
 
-router.post('/players',function(req,res){
-    for(i=0;i<players.length;i++){
-     if (req.body.name == players[i].name){
-           return res.send({data:"ERROR!!!!!!(name should be unique)"})
-        }
-    }
+router.get("/test-api-5", function (req, res) {
+    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
 
-    let c = req.body
-    players.push(c)
-   return res.send({data:players,status:true})
+router.get("/test-api-6", function (req, res) {
+    res.send({ a: 56, b: 45 })
+})
+
+router.post("/test-post", function (req, res) {
+    res.send([23, 45, 6])
+})
+
+
+router.post("/test-post-2", function (req, res) {
+    res.send({ msg: "hi", status: true })
+})
+
+router.post("/test-post-3", function (req, res) {
+    // let id = req.body.user
+    // let pwd= req.body.password
+
+    // console.log( id , pwd)
+
+    console.log(req.body)
+
+    res.send({ msg: "hi", status: true })
+})
+
+
+
+router.post("/test-post-4", function (req, res) {
+    let arr = [12, "functionup"]
+    let ele = req.body.element
+    arr.push(ele)
+    res.send({ msg: arr, status: true })
 })
 
 module.exports = router;
-// adding this comment for no reason
