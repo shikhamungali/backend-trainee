343aba28045fac55e9bb50151b3d72ca9729bcb2
shikha <shikha1081998@gmail.com>
2022-08-13 20:01:10 +0530

    updated

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -8,18 +8,23 @@ const bookSchema = new mongoose.Schema({
     },
     author: {
         type: String,
+        unique:true,
         required: true
     },
-    category: ["Fantasy", "Dystopian", "Romance", "Science Fiction","Historical","Fiction"],
-    year:{
+    category:{
+        type:String,
+        enum: ["Fantasy", "Dystopian", "Romance", "Science Fiction", "Historical", "Fiction"]
+    },
+    year: {
         type: Number,
+        unique:true,
         required: true
     }
 
 }, { timestamps: true });
 
 
-     ///////create a model from bookSchema with a name books in the database////////
+///////create a model from bookSchema with a name books in the database////////
 
 module.exports = mongoose.model('Book', bookSchema) //books
 
