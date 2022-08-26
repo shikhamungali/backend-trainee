89110300776afafd5fa06fda338aae5db0e0c324
shikha <shikha1081998@gmail.com>
2022-08-26 23:56:36 +0530

    jwt- middleware

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,36 +1,34 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-/*
-  Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
-*/
-const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
-  let data = abcd.body;
+
+
+
+/////============================================== CREATING A USER ========================================
+
+const createUser = async function (req, res) {
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  res.send({ msg: savedData });
 };
+
+////=================================== WHEN USER LOGIN =========================================================
 
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
   let password = req.body.password;
 
   let user = await userModel.findOne({ emailId: userName, password: password });
+  if (!(userName&&password))
+    return res.send({
+      status:false,
+      msg: "please enter userName and password"
+    });
   if (!user)
     return res.send({
       status: false,
       msg: "username or the password is not corerct",
     });
-
-  // Once the login is successful, create the jwt token with sign function
-  // Sign function has 2 inputs:
-  // Input 1 is the payload or the object containing data to be set in token
-  // The decision about what data to put in token depends on the business requirement
-  // Input 2 is the secret (This is basically a fixed value only set at the server. This value should be hard to guess)
-  // The same secret will be used to decode tokens 
   let token = jwt.sign(
     {
       userId: user._id.toString(), //////////////// PAYLOAD
@@ -45,57 +43,41 @@ const loginUser = async function (req, res) {
 
 
 
+////============================================ GET USER DATA ===================================================
+
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error. This means the user is not logged in.
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
-
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-
-  // Decoding requires the secret again. 
-  // A token can only be decoded successfully if the same secret was used to create(sign) that token.
-  // And because this token is only known to the server, it can be assumed that if a token is decoded at server then this token must have been issued by the same server in past.
-  let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
-  console.log(decodedToken) ///////return payload with "iat" meaning issued at with the time stamp
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
     return res.send({ status: false, msg: "No such user exists" });
-
   res.send({ status: true, data: userDetails });
-  // Note: Try to see what happens if we change the secret while decoding the token
 };
 
+/////======================================== UPDATE USER DATA =========================================
+
 const updateUser = async function (req, res) {
-  // Do the same steps here:
-  // Check if the token is present
-  // Check if the token present is a valid token
-  // Return a different error message in both these cases
-
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  //Return an error if no user with the given id exists in the db
-  if (!user) {
-    return res.send("No such user exists");
-  }
-
   let userData = req.body;
+  let userId = req.params.userId;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
   res.send({ status: updatedUser, data: updatedUser });
 };
+
+/////================================= DELETING A USER ==========================================================
+
+const deleteUser = async function(req,res){
+  let userId = req.params.userId;
+  let updatedUser = await userModel.findOneAndUpdate(
+    { _id: userId },
+    { isDeleted: true },
+    { new: true }
+  );
+  res.send({ status: true, data: updatedUser });
+}
+
+
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const auth = require('../middleware/auth')
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+///// =============================== users API =========================================
 
 router.post("/users", userController.createUser  )
 
+///=============================================== USER LOGIN API ====================================
+
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+//=============================================== USER DATA API ========================================
 
-router.put("/users/:userId", userController.updateUser)
+router.get("/users/:userId",auth.validations ,userController.getUserData)
+
+///====================================== USER UPDATING API =============================================
+
+router.put("/users/:userId",auth.validations, userController.updateUser)
+
+///========================================= DELETING A USER API ======================================
+
+router.delete('/users/:userId',auth.validations,userController.deleteUser)
 
 module.exports = router;
